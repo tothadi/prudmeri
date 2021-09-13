43ab22456cc37a1fbad2fb5c04111650996b7e98
tothadi <tothadam.onp@gmail.com>
2021-09-13 16:10:25 +0200

    removed unnecessary dependencies

--- backend/bin/app.js
+++ backend/bin/app.js
@@ -4,45 +4,32 @@
   Each is commented and marked with [SH] to make them easy to find
 
  */
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var cors = require('cors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
-
-app.use(favicon(__dirname + '/client/favicon.ico'));
 
 // [SH] Initialise Passport before using the route middleware
 //app.use(passport.initialize());
 
-app.use(express.static(path.join(__dirname, 'client')));
-
-// [SH] Use the API routes when path starts with /api
-app.use('/api', routesApi);
+app.use(express.static(path.join(__dirname, '../client')));
 
 
 // This must be after defining the /api routes
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/index.html'));
+    res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
